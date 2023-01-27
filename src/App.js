6b100dae473c994817ccc4093cfcd06e88bfa20f
Mustapha-Amzil <mustaphaamzil124@gmail.com>
2023-01-27 23:13:09 +0100

    creat react-redux project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BookList from './components/BookList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title'>Books Store</h1>
+      <BookList/>
+    </div>
+  );
+}
+
+export default App;
